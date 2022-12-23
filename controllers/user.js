ac8ce3bdd2a284c005b737359462a06620a7ba1a
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-23 22:22:31 +0100

    Done with getting links

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,14 @@
 const User = require('../models/user');
+const Link = require('../models/link')
 const errorHandler = require('../utils/errorHandler')
 
 const getUserDetails = async (req,res)=>{
     const username = req.params.slug;
     try{
         const user = await User.findOne({username});
-        const {password,_id, updatedAt, ...others} = user._doc;
+        const {password,_id, updatedAt, __v, ...others} = user._doc;
+        const links = await Link.find({username});
+        others['links'] = links;
         res.status(200).json(others)
     }catch(err){
         const {status, ...others} = errorHandler(err);
