79a80544a8fea8614b135383f28d5a9fcc189f60
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-26 21:40:26 +0100

    changed token expiry date to 30 days

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-const maxAge = 1*24*60*60;
+const maxAge = 30*24*60*60;
 const createToken = (id)=>{
     const token = jwt.sign({id}, process.env.JWT_ENCRYPTION_KEY, {expiresIn:maxAge});
     return token;
