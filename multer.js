00bdd9c447b91b9023206d7ac7117d11d0e5481b
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-31 17:50:31 +0100

    Done

--- multer.js
+++ multer.js
@@ -1,10 +1,19 @@
 const multer = require('multer');
+const fs = require('fs');
+const folderName = 'uploads';
 let storage = multer.diskStorage({
+    
     destination:function(req,file,cb){
+        if(!fs.existsSync(folderName)){
+            fs.mkdirSync(folderName);
+        }
         cb(null, 'uploads/');
     },
     filename:function(req,file,cb){
-        cb(null,file.originalname);
+        const uniqueSuffix= Date.now() + '-' + Math.round(Math.random(
+            1000000000
+        ));
+        cb(null,`${file.originalname}-${uniqueSuffix}`);
     }
 })
 let upload = multer({storage:storage});
